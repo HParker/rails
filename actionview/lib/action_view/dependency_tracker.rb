d0b6ee9bbe90fa7bc77d1471b886e766ab4b5352
HParker <HParker@github.com>
2021-06-07 16:46:59 -0700

    Remove erb handler and fully convert to ripper parser

--- actionview/lib/action_view/dependency_tracker.rb
+++ actionview/lib/action_view/dependency_tracker.rb
@@ -2,205 +2,9 @@
 
 require "concurrent/map"
 require "action_view/path_set"
-require "ripper"
+require "action_view/ripper_ast_parser"
 
 module ActionView
-  module RipperASTParser
-    class Node < ::Array
-      attr_reader :type
-
-      def initialize(type, arr, opts = {})
-        @type = type
-        super(arr)
-      end
-
-      def children
-        to_a
-      end
-
-      def inspect
-        typeinfo = type && type != :list ? ':' + type.to_s + ', ' : ''
-        's(' + typeinfo + map(&:inspect).join(", ") + ')'
-      end
-
-      def fcall?
-        type == :command || type == :fcall
-      end
-
-      def fcall_named?(name)
-        fcall? &&
-          self[0].type == :@ident &&
-          self[0][0] == name
-      end
-
-      def argument_nodes
-        raise unless fcall?
-        return [] if self[1].nil?
-        if self[1].last == false || self[1].last.type == :vcall
-          self[1][0...-1]
-        else
-          self[1][0..-1]
-        end
-      end
-
-      def string?
-        type == :string_literal
-      end
-
-      def variable_reference?
-        type == :var_ref
-      end
-
-      def vcall?
-        type == :vcall
-      end
-
-      def call?
-        type == :call
-      end
-
-      def variable_name
-        self[0][0]
-      end
-
-      def call_method_name
-        self.last.first
-      end
-
-      def to_string
-        raise unless string?
-        raise "fixme" unless self[0].type == :string_content
-        raise "fixme" unless self[0][0].type == :@tstring_content
-        self[0][0][0]
-      end
-
-      def hash?
-        type == :bare_assoc_hash || type == :hash
-      end
-
-      def to_hash
-        if type == :bare_assoc_hash
-          hash_from_body(self[0])
-        elsif type == :hash && self[0] == nil
-          {}
-        elsif type == :hash && self[0].type == :assoclist_from_args
-          hash_from_body(self[0][0])
-        else
-          raise "not a hash? #{inspect}"
-        end
-      end
-
-      def hash_from_body(body)
-        body.map do |hash_node|
-          return nil if hash_node.type != :assoc_new
-
-          [hash_node[0], hash_node[1]]
-        end.to_h
-      end
-
-      def symbol?
-        type == :@label || type == :symbol_literal
-      end
-
-      def to_symbol
-        if type == :@label && self[0] =~ /\A(.+):\z/
-          $1.to_sym
-        elsif type == :symbol_literal && self[0].type == :symbol && self[0][0].type == :@ident
-          self[0][0][0].to_sym
-        else
-          raise "not a symbol?: #{self.inspect}"
-        end
-      end
-    end
-
-    class NodeParser < ::Ripper
-      PARSER_EVENTS.each do |event|
-        arity = PARSER_EVENT_TABLE[event]
-        if /_new\z/ =~ event.to_s && arity == 0
-          module_eval(<<-eof, __FILE__, __LINE__ + 1)
-            def on_#{event}(*args)
-              Node.new(:list, args, lineno: lineno(), column: column())
-            end
-          eof
-        elsif /_add(_.+)?\z/ =~ event.to_s
-          module_eval(<<-eof, __FILE__, __LINE__ + 1)
-            begin; undef on_#{event}; rescue NameError; end
-            def on_#{event}(list, item)
-              list.push(item)
-              list
-            end
-          eof
-        else
-          module_eval(<<-eof, __FILE__, __LINE__ + 1)
-            begin; undef on_#{event}; rescue NameError; end
-            def on_#{event}(*args)
-              Node.new(:#{event}, args, lineno: lineno(), column: column())
-            end
-          eof
-        end
-      end
-
-      SCANNER_EVENTS.each do |event|
-        module_eval(<<-End, __FILE__, __LINE__ + 1)
-          def on_#{event}(tok)
-            Node.new(:@#{event}, [tok], lineno: lineno(), column: column())
-          end
-        End
-      end
-    end
-
-    class RenderCallParser < NodeParser
-      attr_reader :render_calls
-
-      METHODS_TO_PARSE = %w(render render_to_string layout)
-
-      def initialize(*args)
-        super
-
-        @render_calls = []
-      end
-
-      private
-
-      def on_fcall(name, *args)
-        on_render_call(super)
-      end
-
-      def on_command(name, *args)
-        on_render_call(super)
-      end
-
-      def on_render_call(node)
-        METHODS_TO_PARSE.each do |method|
-          if node.fcall_named?(method)
-            @render_calls << [method, node]
-            return node
-          end
-        end
-        node
-      end
-
-      def on_arg_paren(content)
-        content
-      end
-
-      def on_paren(content)
-        content
-      end
-    end
-
-    extend self
-
-    def parse_render_nodes(code)
-      parser = RenderCallParser.new(code)
-      parser.parse
-
-      parser.render_calls.group_by(&:first).collect do |method, nodes|
-        [ method.to_sym, nodes.collect { |v| v[1] } ]
-      end.to_h
-    end
-  end
-
   RenderCall = Struct.new(:virtual_path, :locals_keys)
 
   class RenderParser
@@ -234,7 +38,7 @@ module ActionView
       if path.match?("/")
         path
       else
-        "#{directory}/#{path.singularize}"
+        "#{directory}/#{path}"
       end
     end
 
@@ -270,7 +74,11 @@ module ActionView
           options = normalize_args(node[0], nil)
         elsif node.length == 2
           return unless node[1].hash?
-          options = normalize_args(node[0], node[1])
+          if node[1][0][0][0][0].start_with?("partial")
+            options = normalize_args(node[1][0][0][1], node[1])
+          else
+            options = normalize_args(node[0], node[1])
+          end
         end
         return nil unless options
         return parse_render_from_options(options)
@@ -333,6 +141,7 @@ module ActionView
       render_type = (keys & render_type_keys)[0]
 
       node = options_hash[render_type]
+
       if node.string?
         if @from_controller
           template = node.to_string
@@ -486,8 +295,16 @@ module ActionView
     end
 
     class RipperTracker
+      def initialize(name, template, view_paths = nil)
+        @name, @template, @view_paths = name, template, view_paths
+      end
+
       EXPLICIT_DEPENDENCY = /# Template Dependency: (\S+)/
       def self.call(name, template, view_paths = nil)
+        new(name, template, view_paths).dependencies
+      end
+
+      def dependencies
         if template.source.include?("render") || true
           compiled_source = template.handler.call(template, template.source)
           RenderParser.new(name, compiled_source).render_calls + explicit_dependencies(template.source, view_paths)
@@ -500,7 +317,12 @@ module ActionView
         true
       end
 
-      def self.explicit_dependencies(source, view_paths)
+      attr_reader :template, :name, :view_paths
+      private :template, :name, :view_paths
+
+      private
+
+      def explicit_dependencies(source, view_paths)
         dependencies = source.scan(EXPLICIT_DEPENDENCY).flatten.uniq
 
         wildcards, explicits = dependencies.partition { |dependency| dependency.end_with?("/*") }
@@ -508,7 +330,7 @@ module ActionView
         (explicits + resolve_directories(wildcards, view_paths)).uniq
       end
 
-      def self.resolve_directories(wildcard_dependencies, view_paths)
+      def resolve_directories(wildcard_dependencies, view_paths)
         return [] unless view_paths
         return [] if wildcard_dependencies.empty?
 
@@ -518,155 +340,6 @@ module ActionView
         view_paths.flat_map(&:all_template_paths).uniq.filter_map { |path|
           path.to_s if prefixes.include?(path.prefix)
         }.sort
-      end
-    end
-
-    class ERBTracker # :nodoc:
-      EXPLICIT_DEPENDENCY = /# Template Dependency: (\S+)/
-
-      # A valid ruby identifier - suitable for class, method and specially variable names
-      IDENTIFIER = /
-        [[:alpha:]_] # at least one uppercase letter, lowercase letter or underscore
-        [[:word:]]*  # followed by optional letters, numbers or underscores
-      /x
-
-      # Any kind of variable name. e.g. @instance, @@class, $global or local.
-      # Possibly following a method call chain
-      VARIABLE_OR_METHOD_CHAIN = /
-        (?:\$|@{1,2})?            # optional global, instance or class variable indicator
-        (?:#{IDENTIFIER}\.)*      # followed by an optional chain of zero-argument method calls
-        (?<dynamic>#{IDENTIFIER}) # and a final valid identifier, captured as DYNAMIC
-      /x
-
-      # A simple string literal. e.g. "School's out!"
-      STRING = /
-        (?<quote>['"]) # an opening quote
-        (?<static>.*?) # with anything inside, captured as STATIC
-        \k<quote>      # and a matching closing quote
-      /x
-
-      # Part of any hash containing the :partial key
-      PARTIAL_HASH_KEY = /
-        (?:\bpartial:|:partial\s*=>) # partial key in either old or new style hash syntax
-        \s*                          # followed by optional spaces
-      /x
-
-      # Part of any hash containing the :layout key
-      LAYOUT_HASH_KEY = /
-        (?:\blayout:|:layout\s*=>)   # layout key in either old or new style hash syntax
-        \s*                          # followed by optional spaces
-      /x
-
-      # Matches:
-      #   partial: "comments/comment", collection: @all_comments => "comments/comment"
-      #   (object: @single_comment, partial: "comments/comment") => "comments/comment"
-      #
-      #   "comments/comments"
-      #   'comments/comments'
-      #   ('comments/comments')
-      #
-      #   (@topic)         => "topics/topic"
-      #    topics          => "topics/topic"
-      #   (message.topics) => "topics/topic"
-      RENDER_ARGUMENTS = /\A
-        (?:\s*\(?\s*)                                  # optional opening paren surrounded by spaces
-        (?:.*?#{PARTIAL_HASH_KEY}|#{LAYOUT_HASH_KEY})? # optional hash, up to the partial or layout key declaration
-        (?:#{STRING}|#{VARIABLE_OR_METHOD_CHAIN})      # finally, the dependency name of interest
-      /xm
-
-      LAYOUT_DEPENDENCY = /\A
-        (?:\s*\(?\s*)                                  # optional opening paren surrounded by spaces
-        (?:.*?#{LAYOUT_HASH_KEY})                      # check if the line has layout key declaration
-        (?:#{STRING}|#{VARIABLE_OR_METHOD_CHAIN})      # finally, the dependency name of interest
-      /xm
-
-      def self.supports_view_paths? # :nodoc:
-        true
-      end
-
-      def self.call(name, template, view_paths = nil)
-        new(name, template, view_paths).dependencies
-      end
-
-      def initialize(name, template, view_paths = nil)
-        @name, @template, @view_paths = name, template, view_paths
-      end
-
-      def dependencies
-        render_dependencies + explicit_dependencies
-      end
-
-      attr_reader :name, :template
-      private :name, :template
-
-      private
-      def source
-        template.source
-      end
-
-      def directory
-        name.split("/")[0..-2].join("/")
-      end
-
-      def render_dependencies
-        render_dependencies = []
-        render_calls = source.split(/\brender\b/).drop(1)
-
-        render_calls.each do |arguments|
-          add_dependencies(render_dependencies, arguments, LAYOUT_DEPENDENCY)
-          add_dependencies(render_dependencies, arguments, RENDER_ARGUMENTS)
-        end
-
-        render_dependencies.uniq
-      end
-
-      def add_dependencies(render_dependencies, arguments, pattern)
-        arguments.scan(pattern) do
-          match = Regexp.last_match
-          add_dynamic_dependency(render_dependencies, match[:dynamic])
-          add_static_dependency(render_dependencies, match[:static], match[:quote])
-        end
-      end
-
-      def add_dynamic_dependency(dependencies, dependency)
-        if dependency
-          dependencies << "#{dependency.pluralize}/#{dependency.singularize}"
-        end
-      end
-
-      def add_static_dependency(dependencies, dependency, quote_type)
-        if quote_type == '"'
-          # Ignore if there is interpolation
-          return if dependency.include?('#{')
-        end
-
-        if dependency
-          if dependency.include?("/")
-            dependencies << dependency
-          else
-            dependencies << "#{directory}/#{dependency}"
-          end
-        end
-      end
-
-      def resolve_directories(wildcard_dependencies)
-        return [] unless @view_paths
-        return [] if wildcard_dependencies.empty?
-
-        # Remove trailing "/*"
-        prefixes = wildcard_dependencies.map { |query| query[0..-3] }
-
-        @view_paths.flat_map(&:all_template_paths).uniq.filter_map { |path|
-          path.to_s if prefixes.include?(path.prefix)
-        }.sort
-      end
-
-      def explicit_dependencies
-        dependencies = source.scan(EXPLICIT_DEPENDENCY).flatten.uniq
-
-        wildcards, explicits = dependencies.partition { |dependency| dependency.end_with?("/*") }
-
-        (explicits + resolve_directories(wildcards)).uniq
       end
     end
 
--- actionview/test/template/dependency_tracker_test.rb
+++ actionview/test/template/dependency_tracker_test.rb
@@ -14,6 +14,13 @@ class FakeTemplate
 
   def initialize(source, handler = Neckbeard)
     @source, @handler = source, handler
+    if handler == :erb
+      @handler = ActionView::Template::Handlers::ERB.new
+    end
+  end
+
+  def type
+    ["xml/html"]
   end
 end
 
@@ -48,128 +55,152 @@ class DependencyTrackerTest < ActionView::TestCase
   end
 end
 
-class ERBTrackerTest < Minitest::Test
+class RipperTrackerTest < Minitest::Test
   def make_tracker(name, template)
-    ActionView::DependencyTracker::ERBTracker.new(name, template)
+    ActionView::DependencyTracker::RipperTracker.new(name, template)
   end
 
   def test_dependency_of_erb_template_with_number_in_filename
-    template = FakeTemplate.new("<%# render 'messages/message123' %>", :erb)
+    template = FakeTemplate.new("<%= render 'messages/message123' %>", :erb)
     tracker = make_tracker("messages/_message123", template)
 
-    assert_equal ["messages/message123"], tracker.dependencies
+    dependencies = tracker.dependencies.map { |d| d.virtual_path.gsub(%r|/_|, "/") }.sort
+
+    assert_equal ["messages/message123"], dependencies
   end
 
   def test_dependency_of_template_partial_with_layout
-    template = FakeTemplate.new("<%# render partial: 'messages/show', layout: 'messages/layout' %>", :erb)
+    template = FakeTemplate.new("<%= render partial: 'messages/show', layout: 'messages/layout' %>", :erb)
     tracker = make_tracker("multiple/_dependencies", template)
 
-    assert_equal ["messages/layout", "messages/show"], tracker.dependencies
+    dependencies = tracker.dependencies.map { |d| d.virtual_path.gsub(%r|/_|, "/") }.sort
+
+    assert_equal ["messages/layout", "messages/show"], dependencies
   end
 
   def test_dependency_of_template_layout_standalone
-    template = FakeTemplate.new("<%# render layout: 'messages/layout' do %>", :erb)
+    template = FakeTemplate.new("<%= render layout: 'messages/layout' do %>", :erb)
     tracker = make_tracker("messages/layout", template)
 
-    assert_equal ["messages/layout"], tracker.dependencies
+    dependencies = tracker.dependencies.map { |d| d.virtual_path.gsub(%r|/_|, "/") }.sort
+
+    assert_equal ["messages/layout"], dependencies
   end
 
   def test_finds_dependency_in_correct_directory
-    template = FakeTemplate.new("<%# render(message.topic) %>", :erb)
+    template = FakeTemplate.new("<%= render(message.topic) %>", :erb)
     tracker = make_tracker("messages/_message", template)
 
-    assert_equal ["topics/topic"], tracker.dependencies
+    dependencies = tracker.dependencies.map { |d| d.virtual_path.gsub(%r|/_|, "/") }.sort
+
+    assert_equal ["topics/topic"], dependencies
   end
 
   def test_finds_dependency_in_correct_directory_with_underscore
-    template = FakeTemplate.new("<%# render(message_type.messages) %>", :erb)
+    template = FakeTemplate.new("<%= render(message_type.messages) %>", :erb)
     tracker = make_tracker("message_types/_message_type", template)
 
-    assert_equal ["messages/message"], tracker.dependencies
+    dependencies = tracker.dependencies.map { |d| d.virtual_path.gsub(%r|/_|, "/") }.sort
+
+    assert_equal ["messages/message"], dependencies
   end
 
   def test_dependency_of_erb_template_with_no_spaces_after_render
-    template = FakeTemplate.new("<%# render'messages/message' %>", :erb)
+    template = FakeTemplate.new("<%= render'messages/message' %>", :erb)
     tracker = make_tracker("messages/_message", template)
 
-    assert_equal ["messages/message"], tracker.dependencies
+    dependencies = tracker.dependencies.map { |d| d.virtual_path.gsub(%r|/_|, "/") }.sort
+
+    assert_equal ["messages/message"], dependencies
   end
 
   def test_finds_no_dependency_when_render_begins_the_name_of_an_identifier
-    template = FakeTemplate.new("<%# rendering 'it useless' %>", :erb)
+    template = FakeTemplate.new("<%= rendering 'it useless' %>", :erb)
     tracker = make_tracker("resources/_resource", template)
 
     assert_equal [], tracker.dependencies
   end
 
   def test_finds_no_dependency_when_render_ends_the_name_of_another_method
-    template = FakeTemplate.new("<%# surrender 'to reason' %>", :erb)
+    template = FakeTemplate.new("<%= surrender 'to reason' %>", :erb)
     tracker = make_tracker("resources/_resource", template)
 
     assert_equal [], tracker.dependencies
   end
 
   def test_finds_dependency_on_multiline_render_calls
-    template = FakeTemplate.new("<%#
+    template = FakeTemplate.new("<%=
       render :object => @all_posts,
              :partial => 'posts' %>", :erb)
 
     tracker = make_tracker("some/_little_posts", template)
 
-    assert_equal ["some/posts"], tracker.dependencies
+    dependencies = tracker.dependencies.map { |d| d.virtual_path.gsub(%r|/_|, "/") }.sort
+
+    assert_equal ["some/posts"], dependencies
   end
 
   def test_finds_multiple_unrelated_odd_dependencies
     template = FakeTemplate.new("
-      <%# render('shared/header', title: 'Title') %>
+      <%= render('shared/header', title: 'Title') %>
       <h2>Section title</h2>
-      <%# render@section %>
+      <%= render@section %>
     ", :erb)
 
     tracker = make_tracker("multiple/_dependencies", template)
 
-    assert_equal ["shared/header", "sections/section"], tracker.dependencies
+    dependencies = tracker.dependencies.map { |d| d.virtual_path.gsub(%r|/_|, "/") }.sort
+
+    assert_equal ["sections/section", "shared/header"], dependencies
   end
 
   def test_finds_dependencies_for_all_kinds_of_identifiers
     template = FakeTemplate.new("
-      <%# render $globals %>
-      <%# render @instance_variables %>
-      <%# render @@class_variables %>
+      <%= render $globals %>
+      <%= render @instance_variables %>
+      <%= render @@class_variables %>
     ", :erb)
 
     tracker = make_tracker("identifiers/_all", template)
 
+    dependencies = tracker.dependencies.map { |d| d.virtual_path.gsub(%r|/_|, "/") }.sort
+
     assert_equal [
+      "class_variables/class_variable",
       "globals/global",
-      "instance_variables/instance_variable",
-      "class_variables/class_variable"
-    ], tracker.dependencies
+      "instance_variables/instance_variable"
+    ], dependencies
   end
 
   def test_finds_dependencies_on_method_chains
-    template = FakeTemplate.new("<%# render @parent.child.grandchildren %>", :erb)
+    template = FakeTemplate.new("<%= render @parent.child.grandchildren %>", :erb)
     tracker = make_tracker("method/_chains", template)
 
-    assert_equal ["grandchildren/grandchild"], tracker.dependencies
+    dependencies = tracker.dependencies.map { |d| d.virtual_path.gsub(%r|/_|, "/") }.sort
+
+    assert_equal ["grandchildren/grandchild"], dependencies
   end
 
   def test_finds_dependencies_with_special_characters
-    template = FakeTemplate.new("<%# render @pokémon, partial: 'ピカチュウ' %>", :erb)
+    template = FakeTemplate.new("<%= render @pokémon, partial: 'ピカチュウ' %>", :erb)
     tracker = make_tracker("special/_characters", template)
 
-    assert_equal ["special/ピカチュウ"], tracker.dependencies
+    dependencies = tracker.dependencies.map { |d| d.virtual_path.gsub(%r|/_|, "/") }.sort
+
+    assert_equal ["special/ピカチュウ"], dependencies
   end
 
   def test_finds_dependencies_with_quotes_within
     template = FakeTemplate.new(%{
-      <%# render "single/quote's" %>
-      <%# render 'double/quote"s' %>
+      <%= render "single/quote's" %>
+      <%= render 'double/quote"s' %>
     }, :erb)
 
     tracker = make_tracker("quotes/_single_and_double", template)
 
-    assert_equal ["single/quote's", 'double/quote"s'], tracker.dependencies
+    dependencies = tracker.dependencies.map { |d| d.virtual_path.gsub(%r|/_|, "/") }
+
+    assert_equal ["single/quote's", 'double/quote"s'], dependencies
   end
 
   def test_finds_dependencies_with_extra_spaces
@@ -184,22 +215,26 @@ class ERBTrackerTest < Minitest::Test
 
     tracker = make_tracker("spaces/_extra", template)
 
+    dependencies = tracker.dependencies.map { |d| d.virtual_path.gsub(%r|/_|, "/") }.sort
+
     assert_equal [
-      "spaces/header",
-      "spaces/form",
-      "messages/message",
+      "comments/comment",
       "events/event",
-      "comments/comment"
-    ], tracker.dependencies
+      "messages/message",
+      "spaces/form",
+      "spaces/header"
+    ], dependencies
   end
 
   def test_dependencies_with_interpolation
     template = FakeTemplate.new(%q{
-      <%# render "double/#{quote}" %>
-      <%# render 'single/#{quote}' %>
+      <%= render "double/#{quote}" %>
+      <%= render 'single/#{quote}' %>
     }, :erb)
     tracker = make_tracker("interpolation/_string", template)
 
-    assert_equal ["single/\#{quote}"], tracker.dependencies
+    dependencies = tracker.dependencies.map { |d| d.virtual_path.gsub(%r|/_|, "/") }.sort
+
+    assert_equal ["double/", "single/\#{quote}"], dependencies
   end
 end
